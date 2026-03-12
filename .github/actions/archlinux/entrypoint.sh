03a7b8273bc40e6a979d710baa895d578ebbee6c
Rodrigo Tavares <dev@rodrigotavar.es>
2026-03-12 13:25:22 -0300

    Fix command splitting in arch container

--- .github/actions/archlinux/entrypoint.sh
+++ .github/actions/archlinux/entrypoint.sh
@@ -15,7 +15,7 @@ fi
 
 cd $1
 shift
-sudo -u build --preserve-env=PACKAGER,GPGKEY,REPO_NAME,DELETED_PACKAGES "$@"
+sudo -u build --preserve-env=PACKAGER,GPGKEY,REPO_NAME,DELETED_PACKAGES $@
 
 cd "$od"
 chown -R "$og" .
